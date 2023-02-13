1ad446c39a2dca06bb0fb16dc9cba2542e1141b0
Your Name <claycodes@google.com>
2023-02-13 20:41:58 +0000

    initial commit

--- contentscript.js
+++ contentscript.js
@@ -1,84 +1,82 @@
+/* Copyright 2023 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License. */
+
 /**
- * MIT License
-
-Copyright (c) 2021 Clay Smith
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ * Trigger the check on load of the DOM
  */
-
 document.addEventListener("DOMContentLoaded", function (event) {
-    checkform()
+  checkform();
 });
 
+/**
+ * Check that the form is allowed
+ * @returns void
+ */
 async function checkform() {
-    const domain = await getStorage('domain')
-    let allowlist = await getStorage('allowlist') || []
-    let blocklist = await getStorage('blocklist') || []
-    let blockall = false
+  const domain = await getStorage('domain');
+  let allowlist = await getStorage('allowlist') || [];
+  let blocklist = await getStorage('blocklist') || [];
+  let blockall = false;
 
     if (allowlist && allowlist.length > 0) {
-        allowlist = allowlist.map(e => e.toString().toLowerCase())
+      allowlist = allowlist.map(e => e.toString().toLowerCase());
     }
 
     if (blocklist && blocklist.length > 0) {
-        blocklist = blocklist.map(e => e.toString().toLowerCase())
+      blocklist = blocklist.map(e => e.toString().toLowerCase());
     }
 
     if(domain && blocklist.includes('*')){
-        blockall=true
+      blockall = true;
     }
 
-    let external = document.querySelector('.v1CNvb.sId0Ce')
+  let external = document.querySelector('.v1CNvb.sId0Ce');
     if(!external){
-        external = document.querySelector('.freebirdFormviewerViewFooterDisclaimer')
+      external = document.querySelector('.freebirdFormviewerViewFooterDisclaimer');
     }
-    console.log('.freebirdFormviewerViewFooterDisclaimer',external)
+
     if (!external) {
-        return
+      return;
     }
 
-    const block = external.innerText.includes(chrome.i18n.getMessage('outside')) || external.innerText.includes(chrome.i18n.getMessage('endorsed'))
+  const block = external.innerText.includes(chrome.i18n.getMessage('outside')) || external.innerText.includes(chrome.i18n.getMessage('endorsed'));
 
-    const a = external.querySelectorAll('a')
+  const a = external.querySelectorAll('a');
     const regex = /(?:[\w-]+\.)+[\w-]+/gi;
-    const testformdomain = external.innerText.match(regex)
+  const testformdomain = external.innerText.match(regex);
 
-    let formdomain
+  let formdomain;
     if (testformdomain) {
-        formdomain = testformdomain[0].toString().toLowerCase()
+      formdomain = testformdomain[0].toString().toLowerCase();
     }
 
     function test(e) {
-        return e.includes(formdomain)
+      return e.includes(formdomain);
     }
 
-    console.log('domain',formdomain, 'allowlist', allowlist, allowlist.some(test), 'blocklist',blocklist, blocklist.some(test))
-
     if (allowlist.some(test)) {
-        return
+      return;
     }
 
     if (block || a.length >= 3 || blocklist.some(test) || blockall) {
-        rewrite()
+      rewrite();
     }
 }
 
+/**
+ * Rewrite the site if the fom is blocked
+ */
 function rewrite() {
     newHTML = `<html>
   <head>
@@ -87,19 +85,24 @@ function rewrite() {
   <body>
     <p>This form originates outside the domain and is thereby blocked.</p>
   </body>
-</html>`
+</html>`;
 
-    document.open()
-    document.write(newHTML)
-    document.close()
+  document.open();
+  document.write(newHTML);
+  document.close();
 }
 
+/**
+ * Get the values from Storage
+ * @param {string} item -Storage item
+ * @returns
+ */
 function getStorage(item) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get(item, (res) => {
-            resolve(res[item])
-        })
-    })
+          resolve(res[item]);
+        });
+    });
 }
 
 
